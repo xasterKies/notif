b9c9d9c4f69f643e3131660df03fe107f2043d73
xasterKies <samueledward40@gmail.com>
2022-09-09 19:52:06 +0100

    feat: notif send() for linux

---
+++ notif_linux.go
@@ -0,0 +1,17 @@
+package notif
+
+import "os/exec"
+
+var command = exec.Command
+
+func (n *Notify) Send() error {
+	notifyCmdName := "notify-send"
+
+	notifyCmd, err := exec.LookPath(notifyCmdName)
+	if err != nil {
+		return err
+	}
+
+	notifyCommand := command(notifyCmd, "-u", n.severity.String(), n.title, n.message)
+	return notifyCommand.Run()
+}
